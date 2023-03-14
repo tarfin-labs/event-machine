47dc0e392a97073129234fe7d40d75e20d3b8e41
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-14 13:04:40 +0300

    chore: Fix formatting

--- tests/MachineTest.php
+++ tests/MachineTest.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-use Tarfinlabs\EventMachine\Facades\Machine;
 use Tarfinlabs\EventMachine\State;
+use Tarfinlabs\EventMachine\Facades\Machine;
 
 test('a machine is an instance of State::class', function (): void {
     $machine = Machine::define();
