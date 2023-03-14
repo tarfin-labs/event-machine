6191eda4f958768bdd6316bdd983b7b6bb0534c0
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-14 13:17:54 +0300

    refactor: Rename MachineTest.php to MachineDefinitionTest.php
    The commit changes the name of the MachineTest.php test file to MachineDefinitionTest.php.

--- tests/MachineDefinitionTest.php
+++ tests/MachineDefinitionTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Tarfinlabs\EventMachine\State;
 use Tarfinlabs\EventMachine\Facades\Machine;
 
-test('a machine is an instance of State::class', function (): void {
+test('a Machine is an instance of State::class', function (): void {
     $machine = Machine::define();
 
     expect($machine)->toBeInstanceOf(State::class);
