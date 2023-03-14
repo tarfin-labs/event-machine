96479e8ab4675637bb8de1693f23f924d945a69a
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-14 17:27:31 +0300

    feat: The initial state of a Machine has a parent state as machine itself

--- tests/InitialStateTest.php
+++ tests/InitialStateTest.php
@@ -15,3 +15,15 @@ test('a Machine can have an initial state', function (): void {
         ->initialState->toBeInstanceOf(State::class)
         ->initialState->value->toBe('red');
 });
+
+test('The initial state of a Machine has a parent state as machine itself', function (): void {
+    $machine = Machine::define([
+        'name'          => 'traffic_lights_machine',
+        'initial_state' => 'red',
+    ]);
+
+    expect($machine)
+        ->initialState->parent
+        ->toBeInstanceOf(State::class)
+        ->toBe($machine);
+});
