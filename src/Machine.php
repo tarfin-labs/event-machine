3fb21eebe4489d78aceaf15d6a0cda25c4a06a8e
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-14 13:52:44 +0300

    refactor: make Machine class constructor private and add comment
    The commit modifies the Machine class constructor to be declared as private to prevent direct instantiation of the class. Additionally, a comment is added to explain this change.

--- src/Machine.php
+++ src/Machine.php
@@ -6,6 +6,18 @@ namespace Tarfinlabs\EventMachine;
 
 class Machine
 {
+    /**
+     * Machine class constructor.
+     *
+     * This method is declared as private to prevent direct instantiation of the Machine class.
+     * Instead, it should be called by static factory method ({@see \Tarfinlabs\EventMachine\Facades\Machine::define()}).
+     *
+     * @access private
+     */
+    private function __construct()
+    {
+    }
+
     public static function define(?array $definition = null): State
     {
         return new State(
