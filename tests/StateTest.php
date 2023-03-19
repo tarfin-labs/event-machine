72537c9b619b140ed5557dd0ef0c7fd40cbfddad
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-19 16:07:03 +0300

    refactor: Change test description in StateTest.php
    This commit updates the description of the test case in StateTest.php to better reflect what is being tested. The test now verifies that the machine state definitions are properly registered, instead of the machine states themselves. The expectations have also been updated to ensure that the keys of the states array are present and that each value is an instance of the StateDefinition class.

--- tests/StateTest.php
+++ tests/StateTest.php
@@ -3,8 +3,9 @@
 declare(strict_types=1);
 
 use Tarfinlabs\EventMachine\Machine;
+use Tarfinlabs\EventMachine\StateDefinition;
 
-it('should properly register machine states', function (): void {
+it('should properly register machine state definitions', function (): void {
     $machine = Machine::define([
         'initial' => 'green',
         'states'  => [
@@ -44,5 +45,7 @@ it('should properly register machine states', function (): void {
         ],
     ]);
 
-    expect($machine->states)->toHaveKeys(['green', 'yellow', 'red']);
+    expect($machine->states)
+        ->toHaveKeys(['green', 'yellow', 'red'])
+        ->each->toBeInstanceOf(StateDefinition::class);
 });
