c8407210b625d8231a93823d2ddde74557c8a7d7
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-21 02:19:10 +0300

    test: add machine reference to state definition test
    Add a new test to verify that the root state definition of a machine has a reference to the machine itself.

--- tests/StateDefinitionTest.php
+++ tests/StateDefinitionTest.php
@@ -10,3 +10,11 @@ test('state definition is an instance of StateDefinition', function (): void {
 
     expect($machine->root)->toBeInstanceOf(StateDefinition::class);
 });
+
+test('state definition has a machine reference', function (): void {
+    $machine = MachineDefinition::define();
+
+    expect($machine->root)->toHaveProperty('machine');
+    expect($machine->root->machine)->toBeInstanceOf(MachineDefinition::class);
+    expect($machine->root->machine)->toBe($machine);
+});
