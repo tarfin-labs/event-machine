0e6d5465e075ba600f20eeceeb0aa5ef49b62356
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-21 02:00:56 +0300

    chore: add docblocks to MachineDefinition and StateDefinition
    This commit adds PHP docblocks to the MachineDefinition and StateDefinition classes, providing additional context and information about the classes and their methods.

--- src/MachineDefinition.php
+++ src/MachineDefinition.php
@@ -6,18 +6,25 @@ namespace Tarfinlabs\EventMachine;
 
 class MachineDefinition
 {
+    /** The default name for the root machine definition. */
     public const DEFAULT_NAME = '(machine)';
 
+    /** The default delimiter used for constructing the global id by concatenating state definition local IDs. */
     public const STATE_DELIMITER = '.';
 
+    /** The root state definition for this machine definition. */
     public StateDefinition $root;
 
+    /**
+     * @param  array|null  $config The raw configuration array used to create the machine definition.
+     * @param  string  $name The name of the machine.
+     * @param  string|null  $version The version of the machine.
+     * @param  string  $delimiter The string delimiter for serializing the path to a string.
+     */
     private function __construct(
-        /** The raw config used to create the machine. */
         public ?array $config,
         public string $name,
         public ?string $version,
-        /** The string delimiter for serializing the path to a string. */
         public string $delimiter = self::STATE_DELIMITER,
     ) {
         $this->root = new StateDefinition(
@@ -29,6 +36,13 @@ class MachineDefinition
         );
     }
 
+    /**
+     * Define a new machine with the given configuration.
+     *
+     * @param  ?array  $config The raw configuration array used to create the machine.
+     *
+     * @return self The created machine definition.
+     */
     public static function define(
         ?array $config = null,
     ): self {
--- src/StateDefinition.php
+++ src/StateDefinition.php
@@ -6,11 +6,18 @@ namespace Tarfinlabs\EventMachine;
 
 class StateDefinition
 {
-    /** The root machine definition. */
+    /** The root machine definition this state definition belongs to. */
     public MachineDefinition $machine;
 
+    /** The local id of the state definition, representing its location in the overall state value. */
     public string $localId;
 
+    /**
+     * Create a new state definition with the given configuration and options.
+     *
+     * @param  ?array  $config The raw configuration array used to create the state definition.
+     * @param  ?array  $options The options array for configuring the state definition.
+     */
     public function __construct(
         public ?array $config,
         ?array $options = null,
