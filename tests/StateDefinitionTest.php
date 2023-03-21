d8a03ee24c324de7726ef9e962e5741e63c4ec3c
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-21 22:30:07 +0300

    test: Improve test for parent state definition on StateDefinition
    Improved the existing test for parent state definition on StateDefinition in the StateDefinitionTest suite.

--- tests/StateDefinitionTest.php
+++ tests/StateDefinitionTest.php
@@ -53,6 +53,14 @@ test('state definition has a null config when not provided', function (): void {
     expect($machine->root->config)->toBeNull();
 });
 
+test('state definition has a parent state definition', function (): void {
+    // TODO: This test can be written better without using the root state definition and without expecting a null parent
+    $machine = MachineDefinition::define();
+
+    expect($machine->root)->toHaveProperty('parent');
+    expect($machine->root->parent)->toBeNull();
+});
+
 test('state definition has a path', function (): void {
     // TODO: This test can be written better without using the root state definition
     $machine = MachineDefinition::define();
