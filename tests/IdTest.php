f41175591448c46c94381d147accbe75e6759178
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-22 23:09:28 +0300

    test: add additional test for state definition ID assignment
    This commit adds an additional test to ensure that the state definition ID is correctly assigned when defining a machine definition.

--- tests/IdTest.php
+++ tests/IdTest.php
@@ -4,13 +4,26 @@ declare(strict_types=1);
 
 use Tarfinlabs\EventMachine\MachineDefinition;
 
-test('id represent root state definition', function (): void {
+test('it should correctly assign machine definition id', function (): void {
     $idMachine = MachineDefinition::define(config: [
-        'id' => 'some-id',
+        'id'     => 'some-id',
         'states' => [
             'idle' => [],
         ],
     ]);
 
     expect($idMachine->id)->toBe('some-id');
+});
+
+test('it should correctly assign state definition id', function (): void {
+    $idMachine = MachineDefinition::define(config: [
+        'id'     => 'some-id',
+        'states' => [
+            'idle' => [
+                'id' => 'idle-id',
+            ],
+        ],
+    ]);
+
+    expect($idMachine->states['idle']->id)->toBe('idle-id');
 });
