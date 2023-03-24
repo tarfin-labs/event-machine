c316d2aeb69751f204a32dda8da68eda6460cdd2
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-24 11:50:59 +0300

    test: move the test to StateDefinitionTest and remove the StateDefinitionTransitionTest

--- tests/StateDefinitionTest.php
+++ tests/StateDefinitionTest.php
@@ -186,3 +186,55 @@ test('a state config can be null', function (): void {
         ->toHaveKeys(['green', 'yellow', 'red'])
         ->each->toBeInstanceOf(StateDefinition::class);
 });
+
+test('a state definition can have transitions', function (): void {
+    $lightMachine = MachineDefinition::define([
+        'id'      => 'light-machine',
+        'initial' => 'green',
+        'states'  => [
+            'green' => [
+                'on' => [
+                    'TIMER'           => 'yellow',
+                    'POWER_OUTAGE'    => 'red',
+                    'FORBIDDEN_EVENT' => null,
+                ],
+            ],
+            'yellow' => [
+                'on' => [
+                    'TIMER'        => 'red',
+                    'POWER_OUTAGE' => 'red',
+                ],
+            ],
+            'red' => [
+                'on' => [
+                    'TIMER'        => 'green',
+                    'POWER_OUTAGE' => [
+                        'target' => 'red',
+                    ],
+                ],
+                'initial' => 'walk',
+                'states'  => [
+                    'walk' => [
+                        'on' => [
+                            'PED_COUNTDOWN' => 'wait',
+                        ],
+                    ],
+                    'wait' => [
+                        'on' => [
+                            'PED_COUNTDOWN' => 'stop',
+                        ],
+                    ],
+                    'stop' => [],
+                ],
+            ],
+        ],
+    ]);
+
+    expect($lightMachine->states['green']->transitions)->each->toBeInstanceOf(TransitionDefinition::class);
+    expect($lightMachine->states['yellow']->transitions)->each->toBeInstanceOf(TransitionDefinition::class);
+    expect($lightMachine->states['red']->transitions)->each->toBeInstanceOf(TransitionDefinition::class);
+    expect($lightMachine->states['red']->states['walk']->transitions)->each->toBeInstanceOf(TransitionDefinition::class);
+    expect($lightMachine->states['red']->states['wait']->transitions)->each->toBeInstanceOf(TransitionDefinition::class);
+    expect($lightMachine->states['red']->states['stop']->transitions)->each->toBeInstanceOf(TransitionDefinition::class);
+});
+
