0cf87f91e9bd7197c70c9475b0c0abb36fc24957
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-25 10:45:47 +0300

    test: a sub state definition can have initial state

--- tests/InitialStateTest.php
+++ tests/InitialStateTest.php
@@ -23,3 +23,36 @@ test('a machine definition has initial state', function (): void {
     expect($machine->states['yellow']->initialState)->toBeNull();
     expect($machine->states['red']->initialState)->toBeNull();
 });
+
+test('a sub state definition can have initial state', function (): void {
+    $machine = MachineDefinition::define(config: [
+        'initial' => 'green',
+        'states'  => [
+            'green'  => [
+                'initial' => 'walk',
+                'states'  => [
+                    'walk'  => [],
+                    'wait'  => [],
+                    'stop'  => [],
+                ],
+            ],
+            'yellow' => [],
+            'red'    => [],
+        ],
+    ]);
+
+    expect($machine->initialState)
+        ->toBeInstanceOf(StateDefinition::class)
+        ->key->toBe('green');
+
+    expect($machine->states['green']->initialState)
+        ->toBeInstanceOf(StateDefinition::class)
+        ->key->toBe('walk');
+
+    expect($machine->states['yellow']->initialState)->toBeNull();
+    expect($machine->states['red']->initialState)->toBeNull();
+
+    expect($machine->states['green']->states['walk']->initialState)->toBeNull();
+    expect($machine->states['green']->states['wait']->initialState)->toBeNull();
+    expect($machine->states['green']->states['stop']->initialState)->toBeNull();
+});
