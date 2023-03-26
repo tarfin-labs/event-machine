195ab3d5467090e2783dff687043af99a98b4af2
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-26 17:18:16 +0300

    refactor: use named parameters in ContextDefinitionTest.php
    Use named parameters instead of positional parameters to improve code readability in ContextDefinitionTest.php file.
    
    This commit replaces the positional parameters with named parameters in the test functions of the ContextDefinitionTest.php file.

--- tests/ContextDefinitionTest.php
+++ tests/ContextDefinitionTest.php
@@ -7,44 +7,44 @@ use Tarfinlabs\EventMachine\ContextDefinition;
 it('can set and get context definition data', function (): void {
     $context = new ContextDefinition();
 
-    $context->set('key1', 'value1');
-    $context->set('key2', 'value2');
+    $context->set(key: 'key1', value: 'value1');
+    $context->set(key: 'key2', value: 'value2');
 
-    expect($context->get('key1'))->toBe('value1');
-    expect($context->get('key2'))->toBe('value2');
+    expect($context->get(key: 'key1'))->toBe('value1');
+    expect($context->get(key: 'key2'))->toBe('value2');
 });
 
 it('returns null for non-existent keys', function (): void {
     $context = new ContextDefinition();
 
-    expect($context->get('non_existent_key'))->toBeNull();
+    expect($context->get(key: 'non_existent_key'))->toBeNull();
 });
 
 it('can check if a key exists', function (): void {
     $context = new ContextDefinition();
-    $context->set('key1', 'value1');
+    $context->set(key: 'key1', value:  'value1');
 
-    expect($context->has('key1'))->toBeTrue();
-    expect($context->has('non_existent_key'))->toBeFalse();
+    expect($context->has(key: 'key1'))->toBeTrue();
+    expect($context->has(key: 'non_existent_key'))->toBeFalse();
 });
 
 it('can remove a key from context data', function (): void {
     $context = new ContextDefinition();
 
-    $context->set('key1', 'value1');
-    $context->set('key2', 'value2');
-    $context->remove('key1');
+    $context->set(key: 'key1', value: 'value1');
+    $context->set(key: 'key2', value: 'value2');
+    $context->remove(key: 'key1');
 
-    expect($context->has('key1'))->toBeFalse();
-    expect($context->has('key2'))->toBeTrue();
+    expect($context->has(key: 'key1'))->toBeFalse();
+    expect($context->has(key: 'key2'))->toBeTrue();
 });
 
 it('can initialize context data with an array', function (): void {
     $initialData = ['key1' => 'value1', 'key2' => 'value2'];
     $context     = new ContextDefinition($initialData);
 
-    expect($context->get('key1'))->toBe('value1');
-    expect($context->get('key2'))->toBe('value2');
+    expect($context->get(key: 'key1'))->toBe('value1');
+    expect($context->get(key: 'key2'))->toBe('value2');
 });
 
 it('can convert context data to an array', function (): void {
@@ -62,18 +62,18 @@ it('can convert context data to an array', function (): void {
 it('can handle edge cases with empty keys and values', function (): void {
     $context = new ContextDefinition();
 
-    $context->set('', 'empty_key_value');
-    $context->set('empty_value_key', '');
+    $context->set(key: '', value: 'empty_key_value');
+    $context->set(key: 'empty_value_key', value: '');
 
-    expect($context->get(''))->toBe('empty_key_value');
-    expect($context->get('empty_value_key'))->toBe('');
+    expect($context->get(key: ''))->toBe('empty_key_value');
+    expect($context->get(key: 'empty_value_key'))->toBe('');
 
-    expect($context->has(''))->toBeTrue();
-    expect($context->has('empty_value_key'))->toBeTrue();
+    expect($context->has(key: ''))->toBeTrue();
+    expect($context->has(key: 'empty_value_key'))->toBeTrue();
 
-    $context->remove('');
-    expect($context->has(''))->toBeFalse();
-    expect($context->has('empty_value_key'))->toBeTrue();
+    $context->remove(key: '');
+    expect($context->has(key: ''))->toBeFalse();
+    expect($context->has(key: 'empty_value_key'))->toBeTrue();
 
     $contextArray = $context->toArray();
     expect($contextArray)->toHaveCount(1);
