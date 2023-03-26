a9671e2d50f7cc3191e883d906edb205930dc0c2
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-26 17:11:30 +0300

    test: Add test for non-existent keys in ContextDefinition
    Add a new test to ensure that ContextDefinition::get() returns null for non-existent keys.

--- tests/ContextDefinitionTest.php
+++ tests/ContextDefinitionTest.php
@@ -13,3 +13,9 @@ it('can set and get context definition data', function () {
     expect($context->get('key1'))->toBe('value1');
     expect($context->get('key2'))->toBe('value2');
 });
+
+it('returns null for non-existent keys', function () {
+    $context = new ContextDefinition();
+
+    expect($context->get('non_existent_key'))->toBeNull();
+});
