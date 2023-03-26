35a1a3f9f9980637c454f401b48e9aedec7d57d7
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-26 17:07:21 +0300

    test: Add test for setting and getting context definition data
    This commit adds a test for the ContextDefinition class to ensure that it can set and get data using the set() and get() methods. The test creates a new ContextDefinition instance, sets two key-value pairs using set(), and then checks that the values can be retrieved correctly using get().

--- tests/ContextDefinitionTest.php
+++ tests/ContextDefinitionTest.php
@@ -1,3 +1,15 @@
 <?php
 
 declare(strict_types=1);
+
+use Tarfinlabs\EventMachine\ContextDefinition;
+
+it('can set and get context definition data', function () {
+    $context = new ContextDefinition();
+
+    $context->set('key1', 'value1');
+    $context->set('key2', 'value2');
+
+    expect($context->get('key1'))->toBe('value1');
+    expect($context->get('key2'))->toBe('value2');
+});
