93c265846cac4ecf56bd070a4836c1f926a2cc03
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-31 14:22:06 +0300

    feat: Add a new test case to verify guarded actions
    This commit adds a new test case to GuardedActionsTest.php to verify that guarded actions are not executed when the guard condition is not met. The test case uses a state machine definition with a guard condition and a guarded action. It verifies that when the guard condition is not met, the guarded action is not executed, and the state machine's context is not changed.

--- tests/GuardedActionsTest.php
+++ tests/GuardedActionsTest.php
@@ -71,3 +71,73 @@ it('should run the guarded action when the guard condition is met', function ():
     // Ensure that the machine's context has been changed.
     expect($machine->context->get('count'))->toBe(4);
 });
+
+it('should not run the guarded action when the guard condition is not met', function (): void {
+    $machine = MachineDefinition::define(
+        config: [
+            'initial' => 'active',
+            'context' => [
+                'count' => 1,
+            ],
+            'states' => [
+                'active' => [
+                    'on' => [
+                        'MUT' => [
+                            'conditions' => 'isEvenGuard',
+                            'actions'    => 'multiplyByTwoAction',
+                        ],
+                        'INC' => ['actions' => 'incrementAction'],
+                        'DEC' => ['actions' => 'decrementAction'],
+                    ],
+                ],
+            ],
+        ],
+        behavior: [
+            'actions' => [
+                'multiplyByTwoAction' => function (ContextDefinition $context): void {
+                    $context->set('count', $context->get('count') * 2);
+                },
+                'incrementAction' => function (ContextDefinition $context, array $event): void {
+                    $context->set('count', $context->get('count') + 1);
+                },
+                'decrementAction' => function (ContextDefinition $context, array $event): void {
+                    $context->set('count', $context->get('count') - 1);
+                },
+            ],
+            'guards' => [
+                'isEvenGuard' => function (ContextDefinition $context, array $event): bool {
+                    return $context->get('count') % 2 === 0;
+                },
+            ],
+        ],
+    );
+
+    $newState = $machine->transition(state: null, event: ['type' => 'MUT']);
+
+    expect($newState)
+        ->toBeInstanceOf(State::class)
+        ->and($newState->value)->toBe(['active']);
+    expect($newState->contextData)->toBe(['count' => 1]);
+
+    // Ensure that the machine's context has not been changed.
+    expect($machine->context->get('count'))->toBe(1);
+
+    $newState = $machine->transition(state: $newState, event: ['type' => 'INC']);
+    expect($newState->contextData)->toBe(['count' => 2]);
+
+    $newState = $machine->transition(state: $newState, event: ['type' => 'DEC']);
+    expect($newState->contextData)->toBe(['count' => 1]);
+
+    $newState = $machine->transition(state: $newState, event: [
+        'type' => 'MUT',
+    ]);
+
+    expect($newState)
+        ->toBeInstanceOf(State::class)
+        ->and($newState->value)->toBe(['active']);
+    // Guard condition not met, action not executed
+    expect($newState->contextData)->toBe(['count' => 1]);
+
+    // Ensure that the machine's context has not been changed.
+    expect($machine->context->get('count'))->toBe(1);
+});
