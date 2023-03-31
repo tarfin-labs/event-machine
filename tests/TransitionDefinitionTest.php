5028e92e83e653b3c08d45b55f7fe5e100501e0d
Yunus Emre Deligöz <ye@deligoz.me>
2023-03-31 10:43:48 +0300

    chore(TransitionDefinitionTest): reorder tests

--- tests/TransitionDefinitionTest.php
+++ tests/TransitionDefinitionTest.php
@@ -4,6 +4,70 @@ declare(strict_types=1);
 
 use Tarfinlabs\EventMachine\MachineDefinition;
 
+test('single actions can be defined as strings instead of arrays', function (): void {
+    $machine = MachineDefinition::define(config: [
+        'states' => [
+            'green' => [
+                'on' => [
+                    'TIMER' => [
+                        'target'  => 'yellow',
+                        'actions' => 'action1',
+                    ],
+                ],
+            ],
+            'yellow' => [],
+        ],
+    ]);
+
+    $timerTransition = $machine->states['green']->transitions['TIMER'];
+
+    expect($timerTransition->actions)->toBe(['action1']);
+});
+
+test('transitions can have decriptions', function (): void {
+    $machine = MachineDefinition::define(config: [
+        'states' => [
+            'green' => [
+                'on' => [
+                    'TIMER' => [
+                        'target'      => 'yellow',
+                        'description' => 'The timer has expired',
+                    ],
+                ],
+            ],
+            'yellow' => [],
+        ],
+    ]);
+
+    $timerTransition = $machine->states['green']->transitions['TIMER'];
+
+    expect($timerTransition->description)->toBe('The timer has expired');
+});
+
+test('the transition target can be null', function (): void {
+    $machine = MachineDefinition::define([
+        'initial' => 'active',
+        'states'  => [
+            'active' => [
+                'on' => [
+                    'INC' => [
+                        'actions' => ['increment'],
+                    ],
+                    'DEC' => [
+                        'actions' => 'decrement',
+                    ],
+                ],
+            ],
+        ],
+    ]);
+
+    $incTransition = $machine->states['active']->transitions['INC'];
+    $decTransition = $machine->states['active']->transitions['DEC'];
+
+    expect($incTransition->target)->toBeNull();
+    expect($decTransition->target)->toBeNull();
+});
+
 test('transitions can have actions', function (): void {
     $machine = MachineDefinition::define(config: [
         'states' => [
@@ -73,68 +137,4 @@ test('a guarded transition can have multiple specified conditions', function ():
         'guard2',
         'guard3',
     ]);
-});
-
-test('single actions can be defined as strings instead of arrays', function (): void {
-    $machine = MachineDefinition::define(config: [
-        'states' => [
-            'green' => [
-                'on' => [
-                    'TIMER' => [
-                        'target'  => 'yellow',
-                        'actions' => 'action1',
-                    ],
-                ],
-            ],
-            'yellow' => [],
-        ],
-    ]);
-
-    $timerTransition = $machine->states['green']->transitions['TIMER'];
-
-    expect($timerTransition->actions)->toBe(['action1']);
-});
-
-test('transitions can have decriptions', function (): void {
-    $machine = MachineDefinition::define(config: [
-        'states' => [
-            'green' => [
-                'on' => [
-                    'TIMER' => [
-                        'target'      => 'yellow',
-                        'description' => 'The timer has expired',
-                    ],
-                ],
-            ],
-            'yellow' => [],
-        ],
-    ]);
-
-    $timerTransition = $machine->states['green']->transitions['TIMER'];
-
-    expect($timerTransition->description)->toBe('The timer has expired');
-});
-
-test('the transition target can be null', function (): void {
-    $machine = MachineDefinition::define([
-        'initial' => 'active',
-        'states'  => [
-            'active' => [
-                'on' => [
-                    'INC' => [
-                        'actions' => ['increment'],
-                    ],
-                    'DEC' => [
-                        'actions' => 'decrement',
-                    ],
-                ],
-            ],
-        ],
-    ]);
-
-    $incTransition = $machine->states['active']->transitions['INC'];
-    $decTransition = $machine->states['active']->transitions['DEC'];
-
-    expect($incTransition->target)->toBeNull();
-    expect($decTransition->target)->toBeNull();
 });
