7b9a1d175108cc23496cca2d2bb015d42f667566
Fatih AYDIN <fatih.aydin@tarfin.com>
2023-06-23 12:27:21 +0300

    test: create guard and action internal event tests

--- tests/EventStoreTest.php
+++ tests/EventStoreTest.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Tarfinlabs\EventMachine\ContextManager;
+use Tarfinlabs\EventMachine\Definition\EventDefinition;
 use Tarfinlabs\EventMachine\Definition\MachineDefinition;
 
 it('stores events', function (): void {
@@ -31,5 +33,80 @@ it('stores events', function (): void {
         'type' => 'RED_TIMER',
     ]);
 
-    expect($newState->history)->toHaveCount(2);
+    expect($newState->history)->toHaveCount(3);
+});
+
+it('stores action events', function (): void {
+    $machine = MachineDefinition::define(
+        config: [
+            'initial' => 'active',
+            'context' => [
+                'count' => 0,
+            ],
+            'states' => [
+                'active' => [
+                    'on' => [
+                        'ADD' => ['actions' => 'additionAction'],
+                    ],
+                ],
+            ],
+        ],
+        behavior: [
+            'actions' => [
+                'additionAction' => function (ContextManager $context, EventDefinition $eventDefinition): void {
+                    $context->set('count', $context->get('count') + $eventDefinition->payload['value']);
+                },
+            ],
+        ]
+    );
+
+    $newState = $machine->transition(state: null, event: [
+        'type'    => 'ADD',
+        'payload' => [
+            'value' => 37,
+        ],
+    ]);
+
+    expect($newState->history->pluck('type')->toArray())
+        ->toHaveCount(4)
+        ->toEqual(['machine.initial', 'ADD', 'action.additionAction.initial', 'action.additionAction.done']);
+});
+
+it('stores guard events', function (): void {
+    $machine = MachineDefinition::define(
+        config: [
+            'initial' => 'active',
+            'context' => [
+                'count' => 1,
+            ],
+            'states' => [
+                'active' => [
+                    'on' => [
+                        'MUT' => [
+                            'guards'  => 'isEvenGuard',
+                            'actions' => 'multiplyByTwoAction',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+        behavior: [
+            'actions' => [
+                'multiplyByTwoAction' => function (ContextManager $context): void {
+                    $context->set('count', $context->get('count') * 2);
+                },
+            ],
+            'guards' => [
+                'isEvenGuard' => function (ContextManager $context): bool {
+                    return $context->get('count') % 2 === 0;
+                },
+            ],
+        ],
+    );
+
+    $newState = $machine->transition(state: null, event: ['type' => 'MUT']);
+
+    expect($newState->history->pluck('type')->toArray())
+        ->toHaveCount(4)
+        ->toEqual(['machine.initial', 'MUT', 'guard.isEvenGuard.initial', 'guard.isEvenGuard.fail']);
 });
