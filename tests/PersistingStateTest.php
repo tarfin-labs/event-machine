217bb6a4390b775f50c6aa0570fe14e174a3e62c
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-09 16:16:12 +0300

    test: Add test for restoring machine state (WEB-3760)
    Expand PersistingStateTest to not only test persisting the state but also restoring it. This ensures consistent behavior for TrafficLightsMachine instances over time based on their history.

--- tests/PersistingStateTest.php
+++ tests/PersistingStateTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Tarfinlabs\EventMachine\Actor\State;
 use Tarfinlabs\EventMachine\Models\MachineEvent;
 use Tarfinlabs\EventMachine\Tests\Stubs\Machines\TrafficLights\TrafficLightsMachine;
 
@@ -12,12 +13,29 @@ it('can persist the machine state', function (): void {
     $machineActor->send(['type' => 'INC']);
     $machineActor->send(['type' => 'INC']);
 
-    $machineActor->persist();
+    $state = $machineActor->persist();
 
     $eventIds = $machineActor->state->history
         ->pluck('id')
         ->map(fn ($key) => ['id' => $key])
         ->toArray();
 
+    expect($state)->toBeInstanceOf(State::class);
     expect($eventIds)->each->toBeInDatabase(MachineEvent::class);
+});
+
+it('can restore the persisted state', function (): void {
+    $machineActor = TrafficLightsMachine::start();
+
+    $machineActor->send(['type' => 'INC']);
+    $machineActor->send(['type' => 'INC']);
+    $machineActor->send(['type' => 'INC']);
+
+    $machineActor->persist();
+
+    $rootEventId = $machineActor->state->history->first()->root_event_id;
+
+    $anotherMachineActor = TrafficLightsMachine::start($rootEventId);
+
+    expect($anotherMachineActor->state->context->data->count)->toBe(3);
 });
