e60a1179cf833c1bda2aff2be602b24341cfb3d7
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-10 22:05:09 +0300

    test: Update ComputedContextAttributesTest to use Optional (WEB-3770)
    Updated the ComputedContextAttributesTest to use the Optional class of Spatie\LaravelData while creating new TrafficLightsContext instances. This provides more contextually rich and adaptive testing scenarios addressing the optional nature of certain traffic light contexts.
    
    Co-authored-by: Fatih AYDIN <aydinfatih52@gmail.com>

--- tests/ComputedContextAttributesTest.php
+++ tests/ComputedContextAttributesTest.php
@@ -2,10 +2,11 @@
 
 declare(strict_types=1);
 
+use Spatie\LaravelData\Optional;
 use Tarfinlabs\EventMachine\Tests\Stubs\Machines\TrafficLights\TrafficLightsContext;
 
 it('can be computed context methods defined', function (): void {
-    $context = new TrafficLightsContext();
+    $context = new TrafficLightsContext(Optional::create(), Optional::create());
 
     expect($context->count)->toBe(1);
     expect($context->isCountEven())->toBeFalse();
