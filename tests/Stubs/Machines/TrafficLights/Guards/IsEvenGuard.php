9dd01650dacfee47a9ae78b012e9872f984dbd44
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-12 11:10:20 +0300

    test: Refactor IsEvenGuard class in TrafficLights test (WEB-3782)
    Refactored IsEvenGuard class to extend GuardBehavior instead of implementing it. This allows for better consistency across all Guard classes. Introduced an optional error message property for flexibility in error handling. Adjusted method signature to directly accept TrafficLightsContext.

--- tests/Stubs/Machines/TrafficLights/Guards/IsEvenGuard.php
+++ tests/Stubs/Machines/TrafficLights/Guards/IsEvenGuard.php
@@ -7,12 +7,14 @@ namespace Tarfinlabs\EventMachine\Tests\Stubs\Machines\TrafficLights\Guards;
 use Tarfinlabs\EventMachine\ContextManager;
 use Tarfinlabs\EventMachine\Behavior\EventBehavior;
 use Tarfinlabs\EventMachine\Behavior\GuardBehavior;
+use Tarfinlabs\EventMachine\Tests\Stubs\Machines\TrafficLights\TrafficLightsContext;
 
-class IsEvenGuard implements GuardBehavior
+class IsEvenGuard extends GuardBehavior
 {
-    public function __invoke(ContextManager $context, EventBehavior $eventBehavior): bool
+    public ?string $errorMessage = 'Count is not even';
+
+    public function __invoke(TrafficLightsContext|ContextManager $context, EventBehavior $eventBehavior): bool
     {
-        /* @var \Tarfinlabs\EventMachine\Tests\Stubs\Machines\TrafficLights\TrafficLightsContext $context */
         return $context->count % 2 === 0;
     }
 }
