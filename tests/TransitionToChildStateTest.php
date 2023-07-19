bc924d7c6c387f203b826e667b89ab28fda12a0a
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-20 00:28:03 +0300

    test: Add tests for transitioning to/from child states with targets as arrays
    This commit adds two tests in the TransitionToChildStateTest.php file, specifically for checking the transition to and from child states when targets are specified as arrays. The update ensures that the machine definition handles this format properly, improving the overall reliability of the state machine functionality.

--- tests/TransitionToChildStateTest.php
+++ tests/TransitionToChildStateTest.php
@@ -27,6 +27,31 @@ it('can transition to a child state', function (): void {
     expect($newState->value)->toBe(['(machine).stateB.subStateOfB']);
 });
 
+it('can transition to a child state, targets as arrays', function (): void {
+    $machine = MachineDefinition::define(config: [
+        'states' => [
+            'stateA' => [
+                'on' => [
+                    'EVENT' => [
+                        'target' => 'stateB.subStateOfB',
+                    ],
+                ],
+            ],
+            'stateB' => [
+                'states' => [
+                    'subStateOfB' => [],
+                ],
+            ],
+        ],
+    ]);
+
+    $newState = $machine->transition(event: [
+        'type' => 'EVENT',
+    ]);
+
+    expect($newState->value)->toBe(['(machine).stateB.subStateOfB']);
+});
+
 it('can transition from a child state', function (): void {
     $machine = MachineDefinition::define(config: [
         'initial' => 'stateB.subStateOfB',
@@ -37,6 +62,32 @@ it('can transition from a child state', function (): void {
                     'subStateOfB' => [
                         'on' => [
                             'EVENT' => 'stateA',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ]);
+
+    $newState = $machine->transition(event: [
+        'type' => 'EVENT',
+    ]);
+
+    expect($newState->value)->toBe(['(machine).stateA']);
+});
+
+it('can transition from a child state, targets as arrays', function (): void {
+    $machine = MachineDefinition::define(config: [
+        'initial' => 'stateB.subStateOfB',
+        'states'  => [
+            'stateA' => [],
+            'stateB' => [
+                'states' => [
+                    'subStateOfB' => [
+                        'on' => [
+                            'EVENT' => [
+                                'target' => 'stateA',
+                            ],
                         ],
                     ],
                 ],
