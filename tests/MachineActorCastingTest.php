4d882fa74200381c1e7fbbc552cf5cd78b0822c8
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-19 11:00:06 +0300

    test: Add json casting test on MachineActor (WEB-3846)
    A new unit test file has been added, MachineActorCastingTest, to verify that an instance of MachineActor can be correctly cast to json. This change ensures the seamless integration of MachineActor objects with systems or libraries that require json format for data exchange or processing.

---
+++ tests/MachineActorCastingTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Tarfinlabs\EventMachine\Tests\Stubs\Machines\Xyz\XyzMachine;
+
+test('MachineActor can be cast to json', function (): void {
+    $machine = XyzMachine::start();
+
+    expect(json_encode($machine, JSON_THROW_ON_ERROR))
+        ->toBe('"'.$machine->state->history->first()->root_event_id.'"');
+});
