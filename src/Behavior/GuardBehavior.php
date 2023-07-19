ea26b4cbfb08fc686fb94de259795a361c7d8978
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-19 18:29:39 +0300

    refactor!: Remove unused code in GuardBehavior.php (WEB-3691)
    The unused method, validateRequiredContext, and its related imports, TransitionDefinition and MissingMachineContextException, have been removed from the GuardBehavior.php in the src/Behavior directory. This method was previously used to validate the required context for a given guard behavior and context manager, however it's no longer needed. This contributes to code cleanliness and maintainability.

--- src/Behavior/GuardBehavior.php
+++ src/Behavior/GuardBehavior.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Tarfinlabs\EventMachine\Behavior;
 
 use Tarfinlabs\EventMachine\ContextManager;
-use Tarfinlabs\EventMachine\Definition\TransitionDefinition;
-use Tarfinlabs\EventMachine\Exceptions\MissingMachineContextException;
 
 abstract class GuardBehavior extends InvokableBehavior
 {
@@ -18,20 +16,4 @@ abstract class GuardBehavior extends InvokableBehavior
         EventBehavior $eventBehavior,
         array $arguments = null,
     ): bool;
-
-    /**
-     * Validates the required context for a given guard behavior and context manager.
-     *
-     * @param  callable|null  $guardBehavior The guard behavior to validate the required context for.
-     * @param  ContextManager  $context The context manager to check for missing context.
-     *
-     * @throws MissingMachineContextException If missing context is detected.
-     */
-    public static function validateRequiredContext(?callable $guardBehavior, ContextManager $context): void
-    {
-        $hasMissingContext = TransitionDefinition::hasMissingContext($guardBehavior, $context);
-        if ($hasMissingContext !== null) {
-            throw MissingMachineContextException::build($hasMissingContext);
-        }
-    }
 }
