08183e7b8b5ae2b78c1449da2023632cbd4c62f3
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-25 11:37:40 +0300

    (WEB-3872) feat: Add MachineEventValidationException class
    A new exception class, MachineEventValidationException, has been added to handle validation errors specifically for machine events. This provides a more explicit way of handling these kind of scenarios.

---
+++ src/Exceptions/MachineEventValidationException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tarfinlabs\EventMachine\Exceptions;
+
+use Illuminate\Validation\ValidationException;
+
+class MachineEventValidationException extends ValidationException
+{
+}
