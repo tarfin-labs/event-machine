47a7bb3ffb0e71fc9d9a55b6a59b2dc498fab8b2
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-30 01:58:48 +0300

    (WEB-3893) feat: Add method to get Behavior class in BehaviorType
    In the BehaviorType enum class, a new method named "getBehaviorClass" was added.
    The purpose of this change was to provide a dynamic way to determine the class name of the
    corresponding behavior, based on the value of the enum at runtime. This would enhance code
    readability and maintainability by reducing the amount of hardcoding required in the application.

--- src/Behavior/BehaviorType.php
+++ src/Behavior/BehaviorType.php
@@ -4,10 +4,27 @@ declare(strict_types=1);
 
 namespace Tarfinlabs\EventMachine\Behavior;
 
+use Tarfinlabs\EventMachine\ContextManager;
+
 enum BehaviorType: string
 {
     case Guard   = 'guards';
     case Action  = 'actions';
     case Event   = 'events';
     case Context = 'context';
+
+    /**
+     * Returns the behavior class based on the current value of $this.
+     *
+     * @return string The class name of the behavior.
+     */
+    public function getBehaviorClass(): string
+    {
+        return match ($this) {
+            self::Guard   => GuardBehavior::class,
+            self::Action  => ActionBehavior::class,
+            self::Event   => EventBehavior::class,
+            self::Context => ContextManager::class,
+        };
+    }
 }
