5aab7cf736561f80b89f4bf55834c2ba5cfde0df
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-30 15:30:47 +0300

    (WEB-3901) refactor: Refactor internal event name generation
    Reformatted the internal event names in the `InternalEvent` enum and added a method to generate event names with machine ID and optional placeholder. This new format and method enhance the readability and flexibility of event name generation.

--- src/Definition/InternalEvent.php
+++ src/Definition/InternalEvent.php
@@ -4,14 +4,32 @@ declare(strict_types=1);
 
 namespace Tarfinlabs\EventMachine\Definition;
 
+use Illuminate\Support\Str;
+
 enum InternalEvent: string
 {
-    case MACHINE_INIT        = 'machine.init';
-    case STATE_INIT          = 'machine.state.%s.init';
-    case ACTION_INIT         = 'machine.action.%s.init';
-    case ACTION_EVENT_RAISED = 'machine.action.%s.event_raised';
-    case ACTION_DONE         = 'machine.action.%s.done';
-    case GUARD_INIT          = 'machine.guard.%s.init';
-    case GUARD_FAIL          = 'machine.guard.%s.fail';
-    case GUARD_PASS          = 'machine.guard.%s.pass';
+    case MACHINE_INIT        = '{+}.init';
+    case STATE_INIT          = '{+}.state.{?}.init';
+    case ACTION_INIT         = '{+}.action.{?}.init';
+    case ACTION_EVENT_RAISED = '{+}.action.{?}.event_raised';
+    case ACTION_DONE         = '{+}.action.{?}.done';
+    case GUARD_INIT          = '{+}.guard.{?}.init';
+    case GUARD_FAIL          = '{+}.guard.{?}.fail';
+    case GUARD_PASS          = '{+}.guard.{?}.pass';
+
+    /**
+     * Generate an internal event name based on the machine ID and an optional placeholder.
+     *
+     * @param  string  $machineId The ID of the machine.
+     * @param  string|null  $placeholder An optional placeholder.
+     *
+     * @return string The generated internal event name.
+     */
+    public function generateInternalEventName(string $machineId, string $placeholder = null): string
+    {
+        return Str::swap([
+            '{+}' => $machineId,
+            '{?}' => Str::of($placeholder)->classBasename()->camel(),
+        ], $this->value);
+    }
 }
