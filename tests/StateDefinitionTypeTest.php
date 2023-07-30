4b8d52780c8a0cf220bb916a3a7b73e8c81f76b7
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-30 23:50:06 +0300

    (WEB-3904) test: Add unit test for final state definition
    Added a unit test to verify that a state definition can be correctly defined as `final`. This ensures the MachineDefinition's state configuration correctly interprets and assigns the 'final' state type.

---
+++ tests/StateDefinitionTypeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use Tarfinlabs\EventMachine\Definition\MachineDefinition;
+use Tarfinlabs\EventMachine\Definition\StateDefinitionType;
+
+test('a state definition can be defined as final', function (): void {
+    $machine = MachineDefinition::define(config: [
+        'initial' => 'yellow',
+        'states'  => [
+            'yellow' => [
+                'type' => 'final',
+            ],
+        ],
+    ]);
+
+    $yellowState = $machine->stateDefinitions['yellow'];
+
+    expect($yellowState->type)->toBe(StateDefinitionType::FINAL);
+});
