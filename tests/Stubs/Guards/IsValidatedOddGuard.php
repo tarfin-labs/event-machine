d77bba719cc70966def5e9d8e716f2051841293d
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-30 22:57:12 +0300

    (WEB-3903) test: Add IsValidatedOddGuard for odd count validation
    Introduced IsValidatedOddGuard class in Stubs/Guards for handling scenarios where an odd count validation is required. This guard checks if the count in the context is odd, enhancing the control on the input data and preventing improper handling of input.

---
+++ tests/Stubs/Guards/IsValidatedOddGuard.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tarfinlabs\EventMachine\Tests\Stubs\Guards;
+
+use Tarfinlabs\EventMachine\ContextManager;
+use Tarfinlabs\EventMachine\Behavior\EventBehavior;
+use Tarfinlabs\EventMachine\Behavior\ValidationGuardBehavior;
+use Tarfinlabs\EventMachine\Tests\Stubs\Machines\TrafficLights\TrafficLightsContext;
+
+class IsValidatedOddGuard extends ValidationGuardBehavior
+{
+    public array $requiredContext = [
+        'counts.oddCount' => 'integer',
+    ];
+    public ?string $errorMessage = 'Count is not odd';
+
+    public function __invoke(
+        TrafficLightsContext|ContextManager $context,
+        EventBehavior $eventBehavior,
+        array $arguments = null
+    ): bool {
+        return $context->count % 2 === 1;
+    }
+}
