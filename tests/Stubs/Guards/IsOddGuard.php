cfe1df7b04aa067b2f2e4aa9ad5d45d1e2f3b723
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-30 22:57:32 +0300

    (WEB-3903) test: Update IsOddGuard to extend from GuardBehavior
    Refactored IsOddGuard to directly extend from GuardBehavior rather than ValidationGuardBehavior. The explicit error message string is no longer necessary as the 'counts.oddCount' validation would be thrown by GuardBehavior check. The change simplifies our code and follows the Single Responsibility Principle of a guard.

--- tests/Stubs/Guards/IsOddGuard.php
+++ tests/Stubs/Guards/IsOddGuard.php
@@ -6,15 +6,14 @@ namespace Tarfinlabs\EventMachine\Tests\Stubs\Guards;
 
 use Tarfinlabs\EventMachine\ContextManager;
 use Tarfinlabs\EventMachine\Behavior\EventBehavior;
-use Tarfinlabs\EventMachine\Behavior\ValidationGuardBehavior;
+use Tarfinlabs\EventMachine\Behavior\GuardBehavior;
 use Tarfinlabs\EventMachine\Tests\Stubs\Machines\TrafficLights\TrafficLightsContext;
 
-class IsOddGuard extends ValidationGuardBehavior
+class IsOddGuard extends GuardBehavior
 {
     public array $requiredContext = [
         'counts.oddCount' => 'integer',
     ];
-    public ?string $errorMessage = 'Count is not odd';
 
     public function __invoke(
         TrafficLightsContext|ContextManager $context,
