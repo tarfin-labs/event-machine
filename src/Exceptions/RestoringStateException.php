e409ad2965a9b55925d60f4e69cd8fa54aba8ac3
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-31 12:15:56 +0300

    (WEB-3905) chore: Add doc comments to RestoringStateException class
    In order to provide a better understanding of the purpose and usage of RestoringStateException class, doc comments have been added. This detail includes explaining what the class represents and its extending to RuntimeException class. A comment has also been added to the build function within the class describing its purpose and return. This improves code readability and future debugging efforts.

--- src/Exceptions/RestoringStateException.php
+++ src/Exceptions/RestoringStateException.php
@@ -6,8 +6,21 @@ namespace Tarfinlabs\EventMachine\Exceptions;
 
 use RuntimeException;
 
+/**
+ * Class RestoringStateException.
+ *
+ * This class represents an exception that is thrown when an error occurs during state restoration.
+ * It extends the RuntimeException class.
+ */
 class RestoringStateException extends RuntimeException
 {
+    /**
+     * Build a new instance of the class with the given error message.
+     *
+     * @param  string  $errorMessage The error message.
+     *
+     * @return self The new instance of the class with the given error message.
+     */
     public static function build(string $errorMessage): self
     {
         return new self($errorMessage);
