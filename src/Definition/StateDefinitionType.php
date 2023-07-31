2e3e9a91fa157ce8f5f322be623f0e79fa8f3aaa
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-31 12:16:04 +0300

    (WEB-3905) chore: Add doc strings to StateDefinitionType class
    Added a docstring to the StateDefinitionType class to improve code understanding. This action enhances code readability and will help clarify the purpose of StateDefinitionType for future developers, particularly that it represents the state definition types for a state machine.

--- src/Definition/StateDefinitionType.php
+++ src/Definition/StateDefinitionType.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Tarfinlabs\EventMachine\Definition;
 
+/**
+ * Class StateDefinitionType.
+ *
+ * This class represents the state definition types for a state machine.
+ */
 enum StateDefinitionType: string
 {
     case ATOMIC   = 'atomic';
