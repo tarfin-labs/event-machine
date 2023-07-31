99885870e9c8adaaffdebb536563091318f4bae7
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-31 12:15:43 +0300

    (WEB-3905) chore: Update comments for MissingMachineContextException class
    Enhanced documentation in the `MissingMachineContextException` class. Comments provide more context about this specific exception type - it occurs when a necessary key is missing in the machine's context. The added 'build' method description explains it returns a new instance of this exception with a unique error message.
    
    Note: The number of characters in the last sentence is 203.

--- src/Exceptions/MissingMachineContextException.php
+++ src/Exceptions/MissingMachineContextException.php
@@ -6,8 +6,21 @@ namespace Tarfinlabs\EventMachine\Exceptions;
 
 use RuntimeException;
 
+/**
+ * Exception class to be thrown when a machine context is missing.
+ *
+ * This exception extends the RuntimeException class and is used to indicate that a specific key is missing in the machine context.
+ * It provides a static build method that creates and returns a new instance of the exception with a custom error message.
+ */
 class MissingMachineContextException extends RuntimeException
 {
+    /**
+     * Builds and returns a new instance of the class.
+     *
+     * @param  string  $key The missing key in context.
+     *
+     * @return self A new instance of the class with the missing key in context.
+     */
     public static function build(string $key): self
     {
         return new self("`{$key}` is missing in context.");
