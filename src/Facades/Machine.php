ccbaf46375268347f9dbbcc0d68ede20c16e0650
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-31 12:14:29 +0300

    (WEB-3905) chore: Update comments for Machine class in Facades
    Added explanatory comments and docblocks for the Machine class in the Facades directory, to enhance code readability and understandability. This provides more context about the Machine Facade class, making it easier for developers to understand its functionality.

--- src/Facades/Machine.php
+++ src/Facades/Machine.php
@@ -7,12 +7,21 @@ namespace Tarfinlabs\EventMachine\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * Class Machine Facade.
+ *
+ * This class extends the Facade class and represents a machine. It provides a static method to get the facade accessor.
+ *
  * @see \Tarfinlabs\EventMachine\Machine
  *
  * @codeCoverageIgnore
  */
 class Machine extends Facade
 {
+    /**
+     * Get the access key for the facade.
+     *
+     * @return string The access key for the facade.
+     */
     protected static function getFacadeAccessor(): string
     {
         return \Tarfinlabs\EventMachine\Definition\MachineDefinition::class;
