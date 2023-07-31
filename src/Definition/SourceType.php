df0ff080bf40972beff45b59229d7180f089db93
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-31 11:29:39 +0300

    (WEB-3905) chore: Add comment for SourceType enum
    Added a comment to explain the purpose of the SourceType enum in the Definition directory. The comment clarifies that the enum represents different potential event source types, providing a useful context for developers working with this code in the future.

--- src/Definition/SourceType.php
+++ src/Definition/SourceType.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Tarfinlabs\EventMachine\Definition;
 
+/**
+ * Enum class representing the possible event source types.
+ */
 enum SourceType: string
 {
     case INTERNAL = 'internal';
