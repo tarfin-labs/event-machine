647fcd7efcd305dd5600d2498071797b38548ea8
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-31 12:14:24 +0300

    (WEB-3905) chore: Add detailed comments to InvalidGuardedTransitionException
    In this commit, we added detailed comments and docblocks to the InvalidGuardedTransitionException class which better explains their purpose and usage. This is intended to improve readability and ease of debugging for developers especially when dealing with state machine validation for guarded transition.

--- src/Exceptions/InvalidGuardedTransitionException.php
+++ src/Exceptions/InvalidGuardedTransitionException.php
@@ -6,8 +6,22 @@ namespace Tarfinlabs\EventMachine\Exceptions;
 
 use LogicException;
 
+/**
+ * Exception class representing an invalid guarded transition.
+ *
+ * These exceptions are thrown when attempting to use validation guard behavior inside guarded transitions.
+ * It contains information about the event and state definition where the error occurred.
+ */
 class InvalidGuardedTransitionException extends LogicException
 {
+    /**
+     * Builds a new instance of the class.
+     *
+     * @param  string  $event The event that triggered the error.
+     * @param  string  $stateDefinition The state definition where the error occurred.
+     *
+     * @return self A new instance of the class with the specified error message.
+     */
     public static function build(string $event, string $stateDefinition): self
     {
         return new self(message: 'Validation Guard Behavior is not allowed inside guarded transitions. '.
