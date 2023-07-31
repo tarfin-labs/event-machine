55687787ad485f2e419794340fff26dc72ad44d7
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-31 10:17:59 +0300

    (WEB-3905) chore: Add detailed PHPDoc to EventDefinition class
    The EventDefinition class now includes detailed PHPDoc. This change was enforced to improve the class's understanding and facilitate ease of maintenance. It clarifies the class's purpose, properties, and methods to augment code readability.

--- src/Definition/EventDefinition.php
+++ src/Definition/EventDefinition.php
@@ -6,8 +6,18 @@ namespace Tarfinlabs\EventMachine\Definition;
 
 use Tarfinlabs\EventMachine\Behavior\EventBehavior;
 
+/**
+ * Class EventDefinition.
+ *
+ * This class is an implementation of the EventBehavior class and represents a definition of an event.
+ */
 class EventDefinition extends EventBehavior
 {
+    /**
+     * Returns the type of the given method.
+     *
+     * @return string The type of the method.
+     */
     public static function getType(): string
     {
         return '(event)';
