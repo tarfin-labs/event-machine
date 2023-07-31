b65c92f2e6b0449791022988107687c25323704b
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-31 12:15:35 +0300

    (WEB-3905) chore: Add comments to MachineValidationException class
    This commit adds detailed comments to the `MachineValidationException` class. The comments detail the the class's specific role and functionality as a handler for validation errors related to the Machine class, and its extension from the ValidationException class.

--- src/Exceptions/MachineValidationException.php
+++ src/Exceptions/MachineValidationException.php
@@ -6,6 +6,12 @@ namespace Tarfinlabs\EventMachine\Exceptions;
 
 use Illuminate\Validation\ValidationException;
 
+/**
+ * Class MachineValidationException.
+ *
+ * This exception is used to handle validation errors specific to the Machine class.
+ * It extends the ValidationException class to provide more specific functionality.
+ */
 class MachineValidationException extends ValidationException
 {
 }
