4346b49fd1461bb9830a4dff2d81b4e658eb522e
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-31 12:15:50 +0300

    (WEB-3905) chore: Improve doc comments in ModelTransformer class
    Updated the comments of the ModelTransformer class to provide better understanding of the Transformer interface implementation. This includes a thorough explanation of the 'transform' method, which transforms a value by returning its 'id' property. This enhancement improves code readability and maintainability.

--- src/Transformers/ModelTransformer.php
+++ src/Transformers/ModelTransformer.php
@@ -7,8 +7,22 @@ namespace Tarfinlabs\EventMachine\Transformers;
 use Spatie\LaravelData\Support\DataProperty;
 use Spatie\LaravelData\Transformers\Transformer;
 
+/**
+ * Class ModelTransformer.
+ *
+ * This class implements the Transformer interface.
+ * It is responsible for transforming a given value by returning its 'id' property.
+ */
 class ModelTransformer implements Transformer
 {
+    /**
+     * Transforms the value of a DataProperty object.
+     *
+     * @param  DataProperty  $property The DataProperty object.
+     * @param  mixed  $value The value to be transformed.
+     *
+     * @return mixed The transformed value.
+     */
     public function transform(DataProperty $property, mixed $value): mixed
     {
         return $value->id;
