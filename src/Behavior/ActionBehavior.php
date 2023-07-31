db98cec090c64678b91e1866c5942ac43dd4dde3
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-31 10:12:15 +0300

    (WEB-3905) chore: Add doc string comments to ActionBehavior class
    This commit introduces detailed commenting to the ActionBehavior class in the Behavior subsystem. These comments aim to enhance code readability by providing a clear explanation of the class's purpose, function, and usage. This change should facilitate easier understanding and contributing to the codebase, especially for new developers.

--- src/Behavior/ActionBehavior.php
+++ src/Behavior/ActionBehavior.php
@@ -6,6 +6,12 @@ namespace Tarfinlabs\EventMachine\Behavior;
 
 use Tarfinlabs\EventMachine\ContextManager;
 
+/**
+ * ActionBehavior class.
+ *
+ * This abstract class extends the InvokableBehavior class. It provides a way to define action behaviors
+ * that can be invoked within a specific context.
+ */
 abstract class ActionBehavior extends InvokableBehavior
 {
     abstract public function __invoke(
