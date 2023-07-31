6a9a48da63b5bdaf9616e06375a9c9090415f5a0
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-31 10:13:12 +0300

    (WEB-3905) chore: Add detailed PHPDoc to ValidationGuardBehavior class
    Extended the PHPDoc for the ValidationGuardBehavior class to include detailed information about the class, its properties, and its methods. The change was made to enhance code readability and maintainability by clearly explaining the class's responsibilities and behavior.

--- src/Behavior/ValidationGuardBehavior.php
+++ src/Behavior/ValidationGuardBehavior.php
@@ -6,10 +6,28 @@ namespace Tarfinlabs\EventMachine\Behavior;
 
 use Tarfinlabs\EventMachine\ContextManager;
 
+/**
+ * An abstract class that represents a validation guard behavior.
+ *
+ * This class extends the GuardBehavior class and provides a basic structure for
+ * implementing validation guard behaviors. It defines a public property `$errorMessage`
+ * that can be used to store an error message and an abstract function `__invoke` that
+ * must be implemented by the child classes.
+ */
 abstract class ValidationGuardBehavior extends GuardBehavior
 {
+    /** @var string|null Holds an error message, which is initially null. */
     public ?string $errorMessage = null;
 
+    /**
+     * Invokes the method.
+     *
+     * @param  ContextManager  $context The context manager.
+     * @param  EventBehavior  $eventBehavior The event behavior.
+     * @param  array|null  $arguments The arguments for the method (optional).
+     *
+     * @return bool Returns true if the method invocation was successful, false otherwise.
+     */
     abstract public function __invoke(
         ContextManager $context,
         EventBehavior $eventBehavior,
