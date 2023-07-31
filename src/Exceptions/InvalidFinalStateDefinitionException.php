4d516d87734f6df236c36e4321da55fda3fa4564
Yunus Emre Deligöz <ye@deligoz.me>
2023-07-31 12:14:15 +0300

    (WEB-3905) chore: Add detailed comments to InvalidFinalStateDefinitionException
    Added descriptive comments and docblocks to InvalidFinalStateDefinitionException class. This provides more context and understanding to developers dealing with state machine final state validation, ultimately leading to better readability and easier debugging.

--- src/Exceptions/InvalidFinalStateDefinitionException.php
+++ src/Exceptions/InvalidFinalStateDefinitionException.php
@@ -6,8 +6,18 @@ namespace Tarfinlabs\EventMachine\Exceptions;
 
 use LogicException;
 
+/**
+ * Represents an exception that is thrown when a final state is incorrectly defined in a state machine.
+ */
 class InvalidFinalStateDefinitionException extends LogicException
 {
+    /**
+     * Creates an instance of the class with an error message indicating that the specified final state should not have child states.
+     *
+     * @param  string  $stateDefinition The definition of the final state.
+     *
+     * @return self The instance of the class with the error message.
+     */
     public static function noChildStates(string $stateDefinition): self
     {
         return new self(message: "The final state `{$stateDefinition}` should not have child states. ".
@@ -15,6 +25,13 @@ class InvalidFinalStateDefinitionException extends LogicException
         );
     }
 
+    /**
+     * Creates an instance of the class with an error message indicating that the specified final state should not have transitions.
+     *
+     * @param  string  $stateDefinition The definition of the final state.
+     *
+     * @return self The instance of the class with the error message.
+     */
     public static function noTransitions(string $stateDefinition): self
     {
         return new self(message: "The final state `{$stateDefinition}` should not have transitions. ".
