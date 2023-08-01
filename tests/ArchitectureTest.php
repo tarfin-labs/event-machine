33708b5979eec0fa9816af39e3e1e34df8444a15
Yunus Emre Deligöz <ye@deligoz.me>
2023-08-02 00:03:40 +0300

    (WEB-3924) test: Update list of debugging functions in architecture test
    The 'will not use debugging functions' test has been updated in tests/ArchitectureTest.php to include 'xr' to the list of debugging functions that should not be used. This change ensures the integrity of our testing and follows the best practices for clean, debug-free code.

--- tests/ArchitectureTest.php
+++ tests/ArchitectureTest.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 it('will not use debugging functions')
-    ->expect(['dd', 'dump', 'ray'])
+    ->expect(['dd', 'dump', 'ray', 'xr'])
     ->each->not->toBeUsed();
 
 it('will use strict types for source files')
