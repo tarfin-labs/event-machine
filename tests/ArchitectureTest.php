cec8d7d58ce9a09644e52e203000a97e986ecb4f
Yunus Emre Deligöz <ye@deligoz.me>
2023-08-01 11:30:39 +0300

    (WEB-3924) Add strict types test in ArchitectureTest.php
    Added a new test to the 'ArchitectureTest.php' to ensure that strict types are being used in the 'EventMachine' source files. This is important for ensuring type safety and preventing potential run time errors.

--- tests/ArchitectureTest.php
+++ tests/ArchitectureTest.php
@@ -5,3 +5,7 @@ declare(strict_types=1);
 it('will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
     ->each->not->toBeUsed();
+
+it('will use strict types for source files')
+    ->expect('Tarfinlabs\EventMachine')
+    ->toUseStrictTypes();
