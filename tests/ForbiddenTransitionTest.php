51750a155808fbeb9905aaed14431bdda2938a62
Yunus Emre Deligöz <ye@deligoz.me>
2023-08-10 02:12:17 +0300

    (WEB-3980) test: Add ForbiddenTransitionTest for deeply nested states
    This commit introduces the ForbiddenTransitionTest, a new unit test checking the behavior of top-level event transitions from deeply nested states to other top-level states. The test ensures that the correct functionality of the 'findTransitionDefinition' method in handling transitions in deeper levels of nested states. This method provides clearer direction and enhances the user navigation experience.

---
+++ tests/ForbiddenTransitionTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use Tarfinlabs\EventMachine\Actor\Machine;
+
+test('Top-level event transition can switch from a deeply nested state to another top-level state', function (): void {
+    $machine = Machine::create([
+        'id'      => 'm',
+        'initial' => 'a.b.c.d',
+        'states'  => [
+            'a' => [
+                'on' => [
+                    '@event' => 'x',
+                ],
+                'states' => [
+                    'b' => [
+                        'states' => [
+                            'c' => [
+                                'states' => [
+                                    'd' => [],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'x' => [],
+        ],
+    ]);
+
+    $machine->start();
+
+    expect($machine->state->value)->toBe(['m.a.b.c.d']);
+
+    $machine->send(['type' => '@event']);
+
+    expect($machine->state->value)->toBe(['m.x']);
+});
