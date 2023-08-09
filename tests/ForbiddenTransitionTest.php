682543f562eece93237115de12c4893dd3b51dc7
Yunus Emre Deligöz <ye@deligoz.me>
2023-08-10 02:12:28 +0300

    (WEB-3980) test: Add nested states override functionality to ForbiddenTransitionTest
    The commit extends ForbiddenTransitionTest to verify that a deeply nested state can override top-level event transition of the parent state. The increase in test coverage ensures the 'findTransitionDefinition' method effectively handles transitions in deeply nested states, contributing to improved user navigation experience.

--- tests/ForbiddenTransitionTest.php
+++ tests/ForbiddenTransitionTest.php
@@ -37,3 +37,41 @@ test('Top-level event transition can switch from a deeply nested state to anothe
 
     expect($machine->state->value)->toBe(['m.x']);
 });
+
+test('Forbidded Transition: Nested state can override top-level event transition defined in parent state', function (): void {
+    $machine = Machine::create([
+        'id'      => 'm',
+        'initial' => 'a.b.c.d',
+        'states'  => [
+            'a' => [
+                'on' => [
+                    '@event' => 'x',
+                ],
+                'states' => [
+                    'b' => [
+                        'states' => [
+                            'c' => [
+                                'states' => [
+                                    'd' => [
+                                        'on' => [
+                                            '@event' => null,
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'x' => [],
+        ],
+    ]);
+
+    $machine->start();
+
+    expect($machine->state->value)->toBe(['m.a.b.c.d']);
+
+    $machine->send(['type' => '@event']);
+
+    expect($machine->state->value)->toBe(['m.a.b.c.d']);
+});
