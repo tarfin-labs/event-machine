489f3be5a75950c3043ab57ea5fdb1fee65aca33
Yunus Emre Deligöz <ye@deligoz.me>
2023-08-11 10:51:10 +0300

    (WEB-3985) test: Add GreenResult class for testing purposes
    Created a new stub class 'GreenResult' to be used in testing scenarios. This was done to improve test coverage in StateDefinitionTypeTest by introducing a different type of final state. This variation ensures we check for diverse possible outcomes, thereby increasing the code's reliability.

---
+++ tests/Stubs/Results/GreenResult.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tarfinlabs\EventMachine\Tests\Stubs\Results;
+
+use Illuminate\Support\Carbon;
+use Tarfinlabs\EventMachine\ContextManager;
+use Tarfinlabs\EventMachine\Behavior\EventBehavior;
+use Tarfinlabs\EventMachine\Behavior\ResultBehavior;
+
+class GreenResult extends ResultBehavior
+{
+    public function __invoke(
+        ContextManager $context,
+        EventBehavior $eventBehavior,
+        array $arguments = null
+    ): Carbon {
+        return now();
+    }
+}
