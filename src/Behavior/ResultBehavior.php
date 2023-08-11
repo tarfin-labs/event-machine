475cbf1f30af196ecd69fda2b2aec145152ae470
Yunus Emre Deligöz <ye@deligoz.me>
2023-08-11 10:50:16 +0300

    (WEB-3985) feat: Add abstract ResultBehavior class
    Added an abstract ResultBehavior class in the Behavior directory for the EventMachine namespace. This new class, as an extension of the InvokableBehavior class, introduces a new behavior that the system can use. The method is meant to be invoked with a ContextManager, EventBehavior, and a set of arguments. The specific purpose of this class should be defined in future commits or implementations.

---
+++ src/Behavior/ResultBehavior.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tarfinlabs\EventMachine\Behavior;
+
+use Tarfinlabs\EventMachine\ContextManager;
+
+abstract class ResultBehavior extends InvokableBehavior
+{
+    abstract public function __invoke(
+        ContextManager $context,
+        EventBehavior $eventBehavior,
+        array $arguments = null,
+    ): mixed;
+}
