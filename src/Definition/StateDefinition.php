30749224be6d95be8e9685b9495249c3d3809302
Yunus Emre Deligöz <ye@deligoz.me>
2023-08-15 12:04:53 +0300

    (WEB-3991) feat: Update initialState handling in StateDefinition class
    The previous version assumed a null value when there was no initialStateDefinitionKey. However, this version considers the root state definition to be the initial state if there is no initialStateDefinitionKey. It also introduced several comments to explain this process, improving readability for future code maintenance. Finally, it adds recursive calls for initial state definition with children, enhancing the flexibility and robustness of the state machine structure.

--- src/Definition/StateDefinition.php
+++ src/Definition/StateDefinition.php
@@ -276,22 +276,30 @@ class StateDefinition
      */
     public function findInitialStateDefinition(): ?StateDefinition
     {
+        // Try to find the initial state definition key in the configuration.
+        // If not found, try to find the first state definition key.
         $initialStateDefinitionKey = $this->config['initial']
             ?? array_key_first($this->stateDefinitions ?? [])
             ?? null;
 
+        // If there is no initial state definition key, then this root state definition is the initial state.
         if ($initialStateDefinitionKey === null) {
-            return null;
+            return $this->order === 0 ? $this : null;
         }
 
+        // The initial state definition key is built by concatenating the root state definition id
         $initialStateDefinitionKey = $this->id.$this->machine->delimiter.$initialStateDefinitionKey;
 
+        // Try to find the initial state definition in the machine's id map.
+        /** @var \Tarfinlabs\EventMachine\Definition\StateDefinition $initialStateDefinition */
         $initialStateDefinition = $this->machine->idMap[$initialStateDefinitionKey] ?? null;
 
         if ($initialStateDefinition === null) {
             return null;
         }
 
+        // If the initial state definition has child state definitions,
+        // then try to find it recursively from child state definitions.
         return (
             is_array($initialStateDefinition->stateDefinitions) &&
             count($initialStateDefinition->stateDefinitions) > 0
