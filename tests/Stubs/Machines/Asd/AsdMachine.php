161a4f2c91942b336981c3a319ba4b580c9a2912
Yunus Emre Nalbant <yunus.nalbant0@gmail.com>
2023-11-14 14:50:33 +0300

    test:  Create `AsdMachine`.

--- tests/Stubs/Machines/Asd/AsdMachine.php
+++ tests/Stubs/Machines/Asd/AsdMachine.php
@@ -5,7 +5,39 @@ declare(strict_types=1);
 namespace Tarfinlabs\EventMachine\Tests\Stubs\Machines\Asd;
 
 use Tarfinlabs\EventMachine\Actor\Machine;
+use Tarfinlabs\EventMachine\Definition\MachineDefinition;
+use Tarfinlabs\EventMachine\Tests\Stubs\Machines\Asd\Events\SEvent;
+use Tarfinlabs\EventMachine\Tests\Stubs\Machines\Asd\Actions\AAction;
+use Tarfinlabs\EventMachine\Tests\Stubs\Machines\Asd\Actions\DAction;
+use Tarfinlabs\EventMachine\Tests\Stubs\Machines\Asd\Actions\SAction;
 
 class AsdMachine extends Machine
 {
+    public static function definition(): MachineDefinition
+    {
+        return MachineDefinition::define(config: [
+            'initial' => 'stateA',
+            'states'  => [
+                'stateA' => [
+                    'on' => [
+                        SEvent::class => [
+                            'target'  => 'stateS',
+                            'actions' => [AAction::class],
+                        ],
+                    ],
+                ],
+                'stateS' => [
+                    'on' => [
+                        '@always' => [
+                            'target'  => 'stateD',
+                            'actions' => [SAction::class],
+                        ],
+                    ],
+                ],
+                'stateD' => [
+                    'entry' => [DAction::class],
+                ],
+            ],
+        ]);
+    }
 }
