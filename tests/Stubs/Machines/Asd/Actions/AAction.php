e5316e558f95ed2947f532b72bb6fe8ed342f5b5
Yunus Emre Nalbant <yunus.nalbant0@gmail.com>
2023-11-14 14:51:23 +0300

    test: Create Action and Event.

---
+++ tests/Stubs/Machines/Asd/Actions/AAction.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tarfinlabs\EventMachine\Tests\Stubs\Machines\Asd\Actions;
+
+use Tarfinlabs\EventMachine\ContextManager;
+use Tarfinlabs\EventMachine\Behavior\EventBehavior;
+use Tarfinlabs\EventMachine\Behavior\ActionBehavior;
+use Tarfinlabs\EventMachine\Tests\Stubs\Models\ModelA;
+
+class AAction extends ActionBehavior
+{
+    public function __invoke(ContextManager $context, EventBehavior $eventBehavior, array $arguments = null): void
+    {
+        ModelA::create([
+            'value' => 'lorem ipsum dolor',
+        ]);
+    }
+}
