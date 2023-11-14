56ab0ac40ba01c6ba5f34602633e928abc344612
Fatih AYDIN <fatih.aydin@tarfin.com>
2023-11-14 13:56:26 +0300

    feat: Added `$isTransactional` property to `EventBehavior`

--- src/Behavior/EventBehavior.php
+++ src/Behavior/EventBehavior.php
@@ -36,6 +36,7 @@ abstract class EventBehavior extends Data
     public function __construct(
         public null|string|Optional $type = null,
         public null|array|Optional $payload = null,
+        public null|bool|Optional $isTransactional = false,
         #[WithoutValidation]
         mixed $actor = null,
         public int|Optional $version = 1,
