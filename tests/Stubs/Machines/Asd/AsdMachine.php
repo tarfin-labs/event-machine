283275d4d0d7ada534a16054b5342cfc57a21847
Fatih AYDIN <fatih.aydin@tarfin.com>
2023-11-14 13:56:51 +0300

    feat: Added `AsdMachine`

---
+++ tests/Stubs/Machines/Asd/AsdMachine.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tarfinlabs\EventMachine\Tests\Stubs\Machines\Asd;
+
+use Tarfinlabs\EventMachine\Actor\Machine;
+
+class AsdMachine extends Machine
+{
+}
