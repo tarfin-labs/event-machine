8b6e4256cdfa4628d6762c195b91559c98dc8dde
Yunus Emre Deligöz <ye@deligoz.me>
2023-12-04 23:23:51 +0300

    refactor: Change parent class of `EventCollection` to `Illuminate\Database\Eloquent\Collection` (WEB-4344)
    This commit modifies the `EventCollection` class in `src/EventCollection.php`. The parent class has been changed from `Illuminate\Support\Collection` to `Illuminate\Database\Eloquent\Collection`.

--- src/EventCollection.php
+++ src/EventCollection.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Tarfinlabs\EventMachine;
 
-use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Collection;
 
 class EventCollection extends Collection
 {
