83cc85392fe7150d8645b2383cfccecb53f1a939
Yunus Emre Deligöz <ye@deligoz.me>
2024-11-01 12:42:19 +0300

    WB-610: Add tests for invokable behaviors
    Introduce unit tests for `TestIncrementAction` and `TestCountGuard` within the `InvokableBehaviorFakeTest` suite. These tests check the fake creation and ensure instances of `MockInterface` are returned.

---
+++ tests/InvokableBehaviorFakeTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tarfinlabs\EventMachine\Tests;
+
+use Mockery\MockInterface;
+use Tarfinlabs\EventMachine\ContextManager;
+use Tarfinlabs\EventMachine\Behavior\GuardBehavior;
+use Tarfinlabs\EventMachine\Behavior\ActionBehavior;
+
+class TestIncrementAction extends ActionBehavior
+{
+    public function __invoke(ContextManager $context): void
+    {
+        $context->set('count', ($context->get('count') ?? 0) + 1);
+    }
+}
+
+class TestCountGuard extends GuardBehavior
+{
+    public function __invoke(ContextManager $context): bool
+    {
+        return $context->get('count') > 0;
+    }
+}
+
+beforeEach(function (): void {
+    $this->testAction = new TestIncrementAction();
+    $this->testGuard  = new TestCountGuard();
+});
+
+afterEach(function (): void {
+    TestIncrementAction::resetFakes();
+    TestCountGuard::resetFakes();
+});
+
+it('can create a fake instance', function (): void {
+    // 1. Act
+    TestIncrementAction::fake();
+    TestCountGuard::fake();
+
+    // 2. Assert
+    expect(TestIncrementAction::isFaked())->toBe(true);
+    expect(TestIncrementAction::getFake())->toBeInstanceOf(MockInterface::class);
+
+    expect(TestCountGuard::isFaked())->toBe(true);
+    expect(TestCountGuard::getFake())->toBeInstanceOf(MockInterface::class);
+});
