fecd3487cff5b0dae263910fee03d277791b4ecd
Yunus Emre Deligöz <ye@deligoz.me>
2024-11-01 12:44:13 +0300

    WB-610: Add faking capability to Fakeable trait
    Introduced methods to create mock instances of a class using Mockery within the Fakeable trait. This allows binding of fake instances for dependency injection, enhancing testability.

--- src/Traits/Fakeable.php
+++ src/Traits/Fakeable.php
@@ -4,4 +4,26 @@ declare(strict_types=1);
 
 namespace Tarfinlabs\EventMachine\Traits;
 
-trait Fakeable {}
+use Mockery;
+use Mockery\MockInterface;
+use Illuminate\Support\Facades\App;
+
+trait Fakeable
+{
+    /** @var array Stores fake instances */
+    private static array $fakes = [];
+
+    /**
+     * Create a fake instance of the behavior.
+     */
+    public static function fake(): MockInterface
+    {
+        $mock = Mockery::mock(static::class);
+
+        static::$fakes[static::class] = $mock;
+
+        App::bind(static::class, fn () => $mock);
+
+        return $mock;
+    }
+}
