97ae50c22af2a83c191d601dface3ec3c2e71c97
Yunus Emre Deligöz <ye@deligoz.me>
2024-11-19 03:00:06 +0300

    WB-707: feat(eventmachine): add resolves behaviors trait
    introduce ResolvesBehaviors trait to manage event behaviors

---
+++ src/Traits/ResolvesBehaviors.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tarfinlabs\EventMachine\Traits;
+
+use Tarfinlabs\EventMachine\Enums\BehaviorType;
+use Tarfinlabs\EventMachine\Behavior\EventBehavior;
+use Tarfinlabs\EventMachine\Exceptions\BehaviorNotFoundException;
+
+trait ResolvesBehaviors
+{
+}
