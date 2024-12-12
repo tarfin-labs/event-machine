861b92f224f024911715c945b697e0e9fab2a387
Yunus Emre Deligöz <ye@deligoz.me>
2024-12-12 22:24:34 +0300

    WB-784: test(stateconfigvalidator): add test for valid root level config

--- tests/StateConfigValidatorTest.php
+++ tests/StateConfigValidatorTest.php
@@ -17,3 +17,19 @@ test('validates root level configuration keys', function (): void {
         exceptionMessage: 'Invalid root level configuration keys: invalid_key, another_invalid. Allowed keys are: id, version, initial, context, states, on, type, meta, entry, exit, description, scenarios_enabled, should_persist, delimiter'
     );
 });
+
+test('accepts valid root level configuration', function (): void {
+    // HINT: This test should contain all possible root level configuration keys
+    expect(fn () => MachineDefinition::define([
+        'id'                => 'machine',
+        'version'           => '1.0.0',
+        'initial'           => 'state_a',
+        'context'           => ['some' => 'data'],
+        'scenarios_enabled' => true,
+        'should_persist'    => true,
+        'delimiter'         => '.',
+        'states'            => [
+            'state_a' => [],
+        ],
+    ]))->not->toThrow(exception: InvalidArgumentException::class);
+});
