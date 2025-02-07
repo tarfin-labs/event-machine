c7a8af9f09470d552bf92f523567fecb3e31e1af
Yunus Emre Deligöz <ye@deligoz.me>
2025-02-07 12:01:17 +0300

    test(commands): add test for MachineConfigValidatorCommand

---
+++ tests/Commands/MachineConfigValidatorCommandTest.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tarfinlabs\EventMachine\Tests\Commands;
+
+use Tarfinlabs\EventMachine\Tests\TestCase;
+
+class MachineConfigValidatorCommandTest extends TestCase {}
