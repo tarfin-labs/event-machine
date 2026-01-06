1079ed918ffbf63d1c3da22f4b7339294ea032c5
Yunus Emre Deligöz <ye@deligoz.me>
2026-01-06 13:46:18 +0300

    feat: add generic type annotations for PHPStan

--- src/EventCollection.php
+++ src/EventCollection.php
@@ -5,5 +5,13 @@ declare(strict_types=1);
 namespace Tarfinlabs\EventMachine;
 
 use Illuminate\Database\Eloquent\Collection;
+use Tarfinlabs\EventMachine\Models\MachineEvent;
 
+/**
+ * @extends Collection<int, MachineEvent>
+ *
+ * @method MachineEvent|null first(callable $callback = null, $default = null)
+ * @method MachineEvent|null last(callable $callback = null, $default = null)
+ * @method MachineEvent|null get(int $key, $default = null)
+ */
 class EventCollection extends Collection {}
