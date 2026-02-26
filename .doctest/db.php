2757198dc3138bed36138e4d971acc3a587872b1
Yunus Emre Deligöz <ye@deligoz.me>
2026-02-26 18:14:57 +0300

    chore(db.php): add SQLite memory DB bootstrap for doctest (in-memory test database)

--- .doctest/db.php
+++ .doctest/db.php
@@ -1,1 +1,12 @@
 <?php
+
+config()->set('database.connections.testing', [
+    'driver'   => 'sqlite',
+    'database' => ':memory:',
+]);
+
+$migration = include __DIR__.'/../database/migrations/create_machine_events_table.php.stub';
+$migration->up();
+
+$archiveMigration = include __DIR__.'/../database/migrations/create_machine_events_archive_table.php.stub';
+$archiveMigration->up();
