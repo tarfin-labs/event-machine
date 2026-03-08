98945c3991c087d575a72bf5d2bbd8eae87a6ace
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-08 12:23:06 +0300

    test(RegionAMultiRaiseAction): add stub for multi-raise event tests

---
+++ tests/Stubs/Machines/Parallel/Actions/RegionAMultiRaiseAction.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tarfinlabs\EventMachine\Tests\Stubs\Machines\Parallel\Actions;
+
+use Tarfinlabs\EventMachine\ContextManager;
+use Tarfinlabs\EventMachine\Behavior\ActionBehavior;
+
+class RegionAMultiRaiseAction extends ActionBehavior
+{
+    public function __invoke(ContextManager $context): void
+    {
+        $context->set('region_a_result', 'processed_by_a');
+        $this->raise(['type' => 'STEP_1_DONE']);
+        $this->raise(['type' => 'STEP_2_DONE']);
+    }
+}
