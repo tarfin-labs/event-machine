4f5d8011618de8aa4cda63e735e2ff164c160b72
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-08 21:29:37 +0300

    refactor(E2ENoRaiseMachine): update action class imports for VerbObject convention (convention compliance)

--- tests/Stubs/Machines/Parallel/E2ENoRaiseMachine.php
+++ tests/Stubs/Machines/Parallel/E2ENoRaiseMachine.php
@@ -6,8 +6,8 @@ namespace Tarfinlabs\EventMachine\Tests\Stubs\Machines\Parallel;
 
 use Tarfinlabs\EventMachine\Actor\Machine;
 use Tarfinlabs\EventMachine\Definition\MachineDefinition;
-use Tarfinlabs\EventMachine\Tests\Stubs\Machines\Parallel\Actions\NoRaiseEntryAction;
-use Tarfinlabs\EventMachine\Tests\Stubs\Machines\Parallel\Actions\RegionBRaiseAction;
+use Tarfinlabs\EventMachine\Tests\Stubs\Machines\Parallel\Actions\SetContextWithoutRaiseAction;
+use Tarfinlabs\EventMachine\Tests\Stubs\Machines\Parallel\Actions\ProcessRegionBAction;
 
 /**
  * Region A sets context but does NOT raise an event.
@@ -38,7 +38,7 @@ class E2ENoRaiseMachine extends Machine
                                 'initial' => 'working',
                                 'states'  => [
                                     'working' => [
-                                        'entry' => NoRaiseEntryAction::class,
+                                        'entry' => SetContextWithoutRaiseAction::class,
                                         'on'    => ['REGION_A_PROCESSED' => 'finished'],
                                     ],
                                     'finished' => ['type' => 'final'],
@@ -48,7 +48,7 @@ class E2ENoRaiseMachine extends Machine
                                 'initial' => 'working',
                                 'states'  => [
                                     'working' => [
-                                        'entry' => RegionBRaiseAction::class,
+                                        'entry' => ProcessRegionBAction::class,
                                         'on'    => ['REGION_B_PROCESSED' => 'finished'],
                                     ],
                                     'finished' => ['type' => 'final'],
