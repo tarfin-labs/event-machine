5349751bf40b4dec24316d8684d8ec953d1e9550
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-08 12:23:06 +0300

    test(RegionADeepContextAction): add stub for deep context merge tests

---
+++ tests/Stubs/Machines/Parallel/Actions/RegionADeepContextAction.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tarfinlabs\EventMachine\Tests\Stubs\Machines\Parallel\Actions;
+
+use Tarfinlabs\EventMachine\ContextManager;
+use Tarfinlabs\EventMachine\Behavior\ActionBehavior;
+
+class RegionADeepContextAction extends ActionBehavior
+{
+    public function __invoke(ContextManager $context): void
+    {
+        $context->set('report', ['findeks' => ['score' => 750, 'provider' => 'kkb']]);
+        $context->set('region_a_result', 'processed_by_a');
+    }
+}
