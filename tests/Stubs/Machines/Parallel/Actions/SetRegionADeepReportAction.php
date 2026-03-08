de59ac40a2cc24258c5a3aa84c3714dd056e7b77
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-08 21:29:29 +0300

    refactor(SetRegionADeepReportAction): rename RegionADeepContextAction to VerbObject convention (convention compliance)

--- tests/Stubs/Machines/Parallel/Actions/SetRegionADeepReportAction.php
+++ tests/Stubs/Machines/Parallel/Actions/SetRegionADeepReportAction.php
@@ -7,7 +7,7 @@ namespace Tarfinlabs\EventMachine\Tests\Stubs\Machines\Parallel\Actions;
 use Tarfinlabs\EventMachine\ContextManager;
 use Tarfinlabs\EventMachine\Behavior\ActionBehavior;
 
-class RegionADeepContextAction extends ActionBehavior
+class SetRegionADeepReportAction extends ActionBehavior
 {
     public function __invoke(ContextManager $context): void
     {
