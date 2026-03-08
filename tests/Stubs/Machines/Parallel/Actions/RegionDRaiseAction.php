ffc413252e5801072b057141da44ddda1f0ccda2
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-08 17:58:01 +0300

    test(RegionDRaiseAction): add raise action for region D (E2E chained parallel support)

---
+++ tests/Stubs/Machines/Parallel/Actions/RegionDRaiseAction.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tarfinlabs\EventMachine\Tests\Stubs\Machines\Parallel\Actions;
+
+use Tarfinlabs\EventMachine\ContextManager;
+use Tarfinlabs\EventMachine\Behavior\ActionBehavior;
+
+class RegionDRaiseAction extends ActionBehavior
+{
+    public function __invoke(ContextManager $context): void
+    {
+        $context->set('region_d_result', 'processed_by_d');
+        $this->raise(['type' => 'REGION_D_PROCESSED']);
+    }
+}
