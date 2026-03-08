4477205bed82bd29e009e2e65d5690ca59611414
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-08 21:29:32 +0300

    refactor(ProcessInternalGoAction): rename RaiseInternalEventAction to VerbObject convention (convention compliance)

--- tests/Stubs/Machines/Parallel/Actions/ProcessInternalGoAction.php
+++ tests/Stubs/Machines/Parallel/Actions/ProcessInternalGoAction.php
@@ -7,7 +7,7 @@ namespace Tarfinlabs\EventMachine\Tests\Stubs\Machines\Parallel\Actions;
 use Tarfinlabs\EventMachine\ContextManager;
 use Tarfinlabs\EventMachine\Behavior\ActionBehavior;
 
-class RaiseInternalEventAction extends ActionBehavior
+class ProcessInternalGoAction extends ActionBehavior
 {
     public function __invoke(ContextManager $context): void
     {
