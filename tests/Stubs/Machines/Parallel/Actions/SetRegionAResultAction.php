4dc77ae81b176f4919bf48f3b580f316abc228f1
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-08 21:29:28 +0300

    refactor(SetRegionAResultAction): rename RegionAEntryAction to VerbObject convention (convention compliance)

--- tests/Stubs/Machines/Parallel/Actions/SetRegionAResultAction.php
+++ tests/Stubs/Machines/Parallel/Actions/SetRegionAResultAction.php
@@ -7,7 +7,7 @@ namespace Tarfinlabs\EventMachine\Tests\Stubs\Machines\Parallel\Actions;
 use Tarfinlabs\EventMachine\ContextManager;
 use Tarfinlabs\EventMachine\Behavior\ActionBehavior;
 
-class RegionAEntryAction extends ActionBehavior
+class SetRegionAResultAction extends ActionBehavior
 {
     public function __invoke(ContextManager $context): void
     {
