c1ba1e44d4faaa565aef0c84a1eac3fcc3f8aa6a
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-08 18:02:31 +0300

    test(FailingEntryAction): add failing entry action stub (throws RuntimeException for onFail testing)

---
+++ tests/Stubs/Machines/Parallel/Actions/FailingEntryAction.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tarfinlabs\EventMachine\Tests\Stubs\Machines\Parallel\Actions;
+
+use RuntimeException;
+use Tarfinlabs\EventMachine\ContextManager;
+use Tarfinlabs\EventMachine\Behavior\ActionBehavior;
+
+/**
+ * Entry action that always throws RuntimeException.
+ *
+ * Used to test parallel region job failure handling and onFail transitions.
+ */
+class FailingEntryAction extends ActionBehavior
+{
+    public function __invoke(ContextManager $context): void
+    {
+        throw new RuntimeException('Simulated API failure in region');
+    }
+}
