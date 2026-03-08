ded092a50c2b9ba1d1e438b535eba2c3b3b3c111
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-08 21:19:28 +0300

    refactor(E2EBasicMachine): rename states to follow naming conventions (convention compliance)

--- tests/Stubs/Machines/Parallel/E2EBasicMachine.php
+++ tests/Stubs/Machines/Parallel/E2EBasicMachine.php
@@ -35,23 +35,23 @@ class E2EBasicMachine extends Machine
                         'onDone' => 'completed',
                         'states' => [
                             'region_a' => [
-                                'initial' => 'working_a',
+                                'initial' => 'working',
                                 'states'  => [
-                                    'working_a' => [
+                                    'working' => [
                                         'entry' => RegionARaiseAction::class,
-                                        'on'    => ['REGION_A_PROCESSED' => 'finished_a'],
+                                        'on'    => ['REGION_A_PROCESSED' => 'finished'],
                                     ],
-                                    'finished_a' => ['type' => 'final'],
+                                    'finished' => ['type' => 'final'],
                                 ],
                             ],
                             'region_b' => [
-                                'initial' => 'working_b',
+                                'initial' => 'working',
                                 'states'  => [
-                                    'working_b' => [
+                                    'working' => [
                                         'entry' => RegionBRaiseAction::class,
-                                        'on'    => ['REGION_B_PROCESSED' => 'finished_b'],
+                                        'on'    => ['REGION_B_PROCESSED' => 'finished'],
                                     ],
-                                    'finished_b' => ['type' => 'final'],
+                                    'finished' => ['type' => 'final'],
                                 ],
                             ],
                         ],
