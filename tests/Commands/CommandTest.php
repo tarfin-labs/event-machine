f5f5e8aa035a6f00985dc9e86ff2336ae646f25e
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-12 17:55:50 +0300

    chore(CommandTest): remove GenerateUmlCommand test (command deleted)

--- tests/Commands/CommandTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Tarfinlabs\EventMachine\Commands\GenerateUmlCommand;
-
-it('it has GenerateUmlCommand', function (): void {
-    $this->assertTrue(class_exists(GenerateUmlCommand::class));
-});
