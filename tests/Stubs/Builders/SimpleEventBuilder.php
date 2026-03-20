52bc8a254dc79b6f19b505d0b6d8f865809c200c
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-20 19:46:48 +0300

    test(SimpleEventBuilder): create stub builder with faker definition and withValue/withName methods (EventBuilder test infrastructure)

---
+++ tests/Stubs/Builders/SimpleEventBuilder.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tarfinlabs\EventMachine\Tests\Stubs\Builders;
+
+use Tarfinlabs\EventMachine\Testing\EventBuilder;
+use Tarfinlabs\EventMachine\Tests\Stubs\Events\SimpleEvent;
+
+class SimpleEventBuilder extends EventBuilder
+{
+    protected function eventClass(): string
+    {
+        return SimpleEvent::class;
+    }
+
+    protected function definition(): array
+    {
+        return [
+            'type'    => SimpleEvent::getType(),
+            'payload' => [
+                'name'  => $this->faker->name(),
+                'value' => $this->faker->numberBetween(1, 100),
+            ],
+            'version' => 1,
+        ];
+    }
+
+    public function withValue(int $value): static
+    {
+        return $this->state(function (array $attributes) use ($value) {
+            $attributes['payload']['value'] = $value;
+
+            return $attributes;
+        });
+    }
+
+    public function withName(string $name): static
+    {
+        return $this->state(function (array $attributes) use ($name) {
+            $attributes['payload']['name'] = $name;
+
+            return $attributes;
+        });
+    }
+}
