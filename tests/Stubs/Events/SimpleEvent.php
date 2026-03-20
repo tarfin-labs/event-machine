81a24cae05dd5e9d9964664f4c23fe5a1e9768d3
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-20 19:48:11 +0300

    test(SimpleEvent): add HasBuilder trait with resolveBuilderClass override (enable Event::builder() discovery)

--- tests/Stubs/Events/SimpleEvent.php
+++ tests/Stubs/Events/SimpleEvent.php
@@ -4,12 +4,24 @@ declare(strict_types=1);
 
 namespace Tarfinlabs\EventMachine\Tests\Stubs\Events;
 
+use Tarfinlabs\EventMachine\Testing\HasBuilder;
 use Tarfinlabs\EventMachine\Behavior\EventBehavior;
+use Tarfinlabs\EventMachine\Tests\Stubs\Builders\SimpleEventBuilder;
 
+/**
+ * @use HasBuilder<SimpleEventBuilder>
+ */
 class SimpleEvent extends EventBehavior
 {
+    use HasBuilder;
+
     public static function getType(): string
     {
         return 'SIMPLE_EVENT';
+    }
+
+    protected static function resolveBuilderClass(): string
+    {
+        return SimpleEventBuilder::class;
     }
 }
