a6da83e5fab1b8b6ffce92a7328ebad72442d28f
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-23 07:36:13 +0300

    refactor(ScenarioController): use ScenarioDiscovery, remove duplicated discovery logic (review fix)

--- src/Scenarios/ScenarioController.php
+++ src/Scenarios/ScenarioController.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Facades\File;
 
 class ScenarioController extends Controller
 {
@@ -51,10 +50,9 @@ class ScenarioController extends Controller
      */
     public function list(): JsonResponse
     {
-        $scenarios = $this->discoverScenarios();
-        $grouped   = [];
+        $grouped = [];
 
-        foreach ($scenarios as $scenarioClass) {
+        foreach (ScenarioDiscovery::discover() as $scenarioClass) {
             /** @var MachineScenario $instance */
             $instance = new $scenarioClass();
             $machine  = class_basename($instance->getMachine());
@@ -99,50 +97,13 @@ class ScenarioController extends Controller
      */
     private function resolveScenarioClass(string $slug): ?string
     {
-        foreach ($this->discoverScenarios() as $class) {
+        foreach (ScenarioDiscovery::discover() as $class) {
             if ($this->classToSlug(class_basename($class)) === $slug) {
                 return $class;
             }
         }
 
         return null;
-    }
-
-    /**
-     * @return array<int, class-string<MachineScenario>>
-     */
-    private function discoverScenarios(): array
-    {
-        $path = config('machine.scenarios.path');
-
-        if (!is_dir($path)) {
-            return [];
-        }
-
-        $scenarios = [];
-
-        foreach (File::allFiles($path) as $file) {
-            $contents  = file_get_contents($file->getPathname());
-            $namespace = null;
-            $class     = null;
-
-            if (preg_match('/namespace\s+([^;]+);/', $contents, $matches)) {
-                $namespace = $matches[1];
-            }
-
-            if (preg_match('/class\s+(\w+)/', $contents, $matches)) {
-                $class = $matches[1];
-            }
-
-            if ($namespace !== null && $class !== null) {
-                $fqcn = $namespace.'\\'.$class;
-                if (class_exists($fqcn) && is_subclass_of($fqcn, MachineScenario::class)) {
-                    $scenarios[] = $fqcn;
-                }
-            }
-        }
-
-        return $scenarios;
     }
 
     private function classToSlug(string $className): string
