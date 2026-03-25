61c3249a1d9bcf2f76725b6a160173dbeb943a0d
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-25 04:55:41 +0300

    test(QueuedExitAction): create queued exit listener stub (sets context flag)

---
+++ tests/Stubs/Listeners/QueuedExitAction.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tarfinlabs\EventMachine\Tests\Stubs\Listeners;
+
+use Tarfinlabs\EventMachine\ContextManager;
+use Tarfinlabs\EventMachine\Behavior\ActionBehavior;
+
+/**
+ * Queued exit listener action that sets a context flag.
+ */
+class QueuedExitAction extends ActionBehavior
+{
+    public function __invoke(ContextManager $context): void
+    {
+        $context->set('exit_listener_ran', true);
+    }
+}
