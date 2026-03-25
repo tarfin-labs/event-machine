902aab6ecb9124af0281e739208f30eb5bc2e813
Yunus Emre Deligöz <ye@deligoz.me>
2026-03-26 01:03:32 +0300

    chore(TrafficLights/Events): remove redundant getType() from 6 convention-matching stubs (auto-generation produces identical result)

--- tests/Stubs/Machines/TrafficLights/Events/IncreaseEvent.php
+++ tests/Stubs/Machines/TrafficLights/Events/IncreaseEvent.php
@@ -6,10 +6,4 @@ namespace Tarfinlabs\EventMachine\Tests\Stubs\Machines\TrafficLights\Events;
 
 use Tarfinlabs\EventMachine\Behavior\EventBehavior;
 
-class IncreaseEvent extends EventBehavior
-{
-    public static function getType(): string
-    {
-        return 'INCREASE';
-    }
-}
+class IncreaseEvent extends EventBehavior {}
